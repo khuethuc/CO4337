320e6d2b23f8a715cd81b281539ac8a42223a168
khuethuc <thuckhue04@gmail.com>
2026-02-09 21:39:18 +0700

    delete hard code part in dataloader dirichlet

--- dataloader_dirichlet.py
+++ dataloader_dirichlet.py
@@ -251,16 +251,6 @@ def partition_trainDataset(dataset_name, data_dir, skew, seed, batch_size,
         return train_set, bsz
 
     # Dirichlet partition + optional Dirichlet attack
-    dirichlet_alpha = 0.3     # normal alpha
-    min_size = 10             # avoid empty client
-    n_classes = 10            # CIFAR10=10, CIFAR100=100, FMNIST=10...
-
-    dirichlet_attack = True   
-    adv_ratio = 0.2           # percentage of malicious client
-    adv_contiguous = True     # contiguous segment on ring topology
-    adv_start = 0             
-    attack_alpha = 0.05       # alpha for attacker (small alpha => high skewness)
-
     labels = _get_labels_cached(dataset, dataset_name)
     client_indices = dirichlet_partition_indices(
         labels=labels,
